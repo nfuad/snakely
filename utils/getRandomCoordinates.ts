57a1949782a2dc8818ba4795d22e50c13445e503
Nafis Fuad <fuadbd14@gmail.com>
2019-12-23 15:08:40 +0600

    Add utility function

---
+++ utils/getRandomCoordinates.ts
@@ -0,0 +1,7 @@
+export default () => {
+  let min = 1
+  let max = 98
+  let x = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2
+  let y = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2
+  return [x, y]
+}
