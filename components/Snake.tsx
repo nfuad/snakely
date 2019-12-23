c61cd340f89fcd372e027c49ed03aaec45c9bb74
Nafis Fuad <fuadbd14@gmail.com>
2019-12-23 15:10:16 +0600

    Update Snake with styles

--- components/Snake.tsx
+++ components/Snake.tsx
@@ -8,7 +8,20 @@ export default (props) => {
           left: `${dot[0]}%`,
           top: `${dot[1]}%`
         }
-        return <div className="snake-dot" key={i} style={style}></div>
+        return (
+          <div className="snake-dot" key={i} style={style}>
+            <style jsx global>{`
+              .snake-dot {
+                position: absolute;
+                width: 2%;
+                height: 2%;
+                background-color: #000;
+                border: 1px solid #fff;
+                z-index: 2;
+              }
+            `}</style>
+          </div>
+        )
       })}
     </div>
   )
