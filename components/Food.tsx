c4aa5752f71e9bbbd094a6c6a05234c047f1a1d0
Nafis Fuad <fuadbd14@gmail.com>
2019-12-23 15:09:40 +0600

    Add food component

--- components/Food.tsx
+++ components/Food.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react'
 
-export default (props) => {
-  const style = {
-    left: `${props.dot[0]}%`,
-    top: `${props.dot[1]}%`
-  }
-
-  return <div className="snake-food" style={style}></div>
-}
+export default (props) => (
+  <div className="food">
+    <style jsx global>
+      {`
+        .food {
+          position: absolute;
+          width: 2%;
+          height: 2%;
+          background: red;
+          border: 1px solid #fff;
+          z-index: 1;
+          left: ${props.dot[0]}%;
+          top: ${props.dot[1]}%;
+        }
+      `}
+    </style>
+  </div>
+)
