b1e5a5cf15be7392ceffe6becfda3ee8dde292f8
Nafis Fuad <fuadbd14@gmail.com>
2019-12-23 11:06:29 +0600

    The dreaded intiial commit

---
+++ components/Food.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export default (props) => {
+  const style = {
+    left: `${props.dot[0]}%`,
+    top: `${props.dot[1]}%`
+  }
+
+  return <div className="snake-food" style={style}></div>
+}
---
+++ pages/index.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react'
+import Game from '../components/Game'
+
+export default () => <Game />
