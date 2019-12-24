bb0fe488f16cf3b5f4f11eab3d56e14849ef0290
Nafis Fuad <fuadbd14@gmail.com>
2019-12-24 21:03:03 +0600

    Add layout and global styles

---
+++ components/Layout.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+
+// custom imports
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
+
+export default (props) => (
+  <>
+    <Nav />
+    {props.children}
+    <Footer />
+    <style jsx global>
+      {`
+        body {
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
+            Helvetica, sans-serif;
+        }
+      `}
+    </style>
+  </>
+)
