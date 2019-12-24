d5fe49aa2b0f369087333ba8a7188255379737a5
Nafis Fuad <fuadbd14@gmail.com>
2019-12-24 21:20:33 +0600

    Update Nav and remove link styles

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,57 +2,48 @@ import React from 'react'
 import Link from 'next/link'
 import GithubCorner from 'react-github-corner'
 
-const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT', key: '' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub', key: '' }
-].map((link) => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
-
-const Nav = () => (
+export default () => (
   <nav>
     <GithubCorner href="https://github.com/nfuad/snakely" />
     <ul>
       <li>
         <Link href="/">
-          <a>Home</a>
+          <a>Game</a>
         </Link>
       </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
+      <li>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+      <li>
+        <a href="https://github.com/nfuad/snakely" target="_blank">
+          Github
+        </a>
+      </li>
     </ul>
 
     <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
       nav {
         text-align: center;
       }
+
       ul {
         display: flex;
         justify-content: space-between;
+        width: 50%;
+        margin: 0 auto;
+        margin-top: 15px;
       }
+
       nav > ul {
         padding: 4px 16px;
       }
+
       li {
         display: flex;
         padding: 6px 8px;
       }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
     `}</style>
   </nav>
 )
-
-export default Nav
