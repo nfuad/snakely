47ecf6bdfde2be86ca9964ddf61a239e3e16e2f4
Nafis Fuad <fuadbd14@gmail.com>
2019-12-27 18:12:20 +0600

    Add Stats in the game board

--- components/Board.tsx
+++ components/Board.tsx
@@ -3,19 +3,24 @@ import * as React from 'react'
 // custom components
 import Snake from './Snake'
 import Food from './Food'
+import Stats from './Stats'
 
 export default ({ data }) => (
-  <div className="paper game-board">
-    <Snake snakeDots={data.snakeDots} />
-    <Food dot={data.food} />
+  <>
+    <div className="paper game-board">
+      <Snake snakeDots={data.snakeDots} />
+      <Food dot={data.food} />
+    </div>
+
+    <Stats points={data.points} paused={data.paused} />
 
     <style jsx global>{`
       .game-board {
         position: relative;
-        margin: 50px auto;
+        margin: 50px 0;
         width: 30vw;
         height: 30vw;
       }
     `}</style>
-  </div>
+  </>
 )
