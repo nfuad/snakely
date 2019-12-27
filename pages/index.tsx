34f84d720a10b419f87774f1e880848ee5d72dec
Nafis Fuad <fuadbd14@gmail.com>
2019-12-27 18:02:29 +0600

    Add how to play component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,11 @@ import * as React from 'react'
 // custom components
 import Game from '../components/Game'
 import Layout from '../components/Layout'
+import HowToPlay from '../components/HowToPlay'
 
 export default () => (
   <Layout>
+    <HowToPlay />
     <Game />
   </Layout>
 )
